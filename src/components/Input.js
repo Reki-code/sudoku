517d496208230977c8a5f2d8b8ffc25b161d7bae
Reki-code <weiruiqi@hotmail.com>
2022-04-13 23:24:30 +0800

    remove Undo button

--- src/components/Input.js
+++ src/components/Input.js
@@ -22,12 +22,6 @@ const Input = ({ className }) => {
         >
           New Game
         </button>
-        <button
-          type='button'
-          className='py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
-        >
-          Undo
-        </button>
         <div className='grid grid-cols-3 grid-rows-3 border'>
           {input.map((num) => (
             <button type='button' onClick={handleClick} className='h-8 w-8' key={num}>
