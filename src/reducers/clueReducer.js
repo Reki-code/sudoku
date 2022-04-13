8ce116591207a874e50fd5203647c1a916b4d307
Yuuta <weiruiqi@hotmail.com>
2022-04-13 15:06:38 +0000

    fix puzzle string to grid map

--- src/reducers/clueReducer.js
+++ src/reducers/clueReducer.js
@@ -31,6 +31,32 @@ const validate = (puzzle, b, c) => {
   }
 }
 
+//
+// [0,0] [0,1] [0,2]  |  [0,3] [0,4] [0,5]  |  [0,6] [0,7] [0,8]
+// [1,0] [1,1] [1,2]  |  [1,3] [1,4] [1,5]  |  [1,6] [1,7] [1,8]
+// [2,0] [2,1] [2,2]  |  [2,3] [2,4] [2,5]  |  [2,6] [2,7] [2,8]
+// -------------------+---------------------+-------------------
+// [3,0] [3,1] [3,2]  |  [3,3] [3,4] [3,5]  |  [3,6] [3,7] [3,8]
+// [4,0] [4,1] [4,2]  |  [4,3] [4,4] [4,5]  |  [4,6] [4,7] [4,8]
+// [5,0] [5,1] [5,2]  |  [5,3] [5,4] [5,5]  |  [5,6] [5,7] [5,8]
+// -------------------+---------------------+-------------------
+// [6,0] [6,1] [6,2]  |  [6,3] [6,4] [6,5]  |  [6,6] [6,7] [6,8]
+// [7,0] [7,1] [7,2]  |  [7,3] [7,4] [7,5]  |  [7,6] [7,7] [7,8]
+// [8,0] [8,1] [8,2]  |  [8,3] [8,4] [8,5]  |  [8,6] [8,7] [8,8]
+const arrayIdx2GridIdx = (idx) => {
+  const row = Math.floor(idx / 9)
+  const col = idx % 9
+
+  const gridRow = Math.floor(row / 3)
+  const gridCol = Math.floor(col / 3)
+  const boxIdx = gridRow * 3 + gridCol
+
+  const boxRow = row % 3
+  const boxCol = col % 3
+  const cellIdx = boxRow * 3 + boxCol
+  return { boxIdx, cellIdx }
+}
+
 const clueSlice = createSlice({
   name: 'clue',
   initialState,
@@ -39,15 +65,16 @@ const clueSlice = createSlice({
       const sudoku = getSudoku('easy')
       sudoku.puzzle.split('')
         .forEach((value, idx) => {
-          const boxIdx = Math.floor(idx / 9)
+          const { boxIdx, cellIdx } = arrayIdx2GridIdx(idx)
+
           if (value !== '-') {
-            state.puzzle[boxIdx][idx % 9].given = true
-            state.puzzle[boxIdx][idx % 9].value = value
+            state.puzzle[boxIdx][cellIdx].given = true
+            state.puzzle[boxIdx][cellIdx].value = value
           } else {
-            state.puzzle[boxIdx][idx % 9].given = false
-            state.puzzle[boxIdx][idx % 9].value = ' '
+            state.puzzle[boxIdx][cellIdx].given = false
+            state.puzzle[boxIdx][cellIdx].value = ' '
           }
-          state.puzzle[boxIdx][idx % 9].solution = sudoku.solution[idx]
+          state.puzzle[boxIdx][cellIdx].solution = sudoku.solution[idx]
         })
       clearColor(state.puzzle)
       state.curCell.boxIdx = -1
