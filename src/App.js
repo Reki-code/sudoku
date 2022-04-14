59a3b3378d31f351bfa14dcf0e11436bf06fcee1
Reki-code <weiruiqi@hotmail.com>
2022-04-14 20:55:45 +0800

    tweak styles

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Input from './components/Input'
 
 const App = () => (
   <div className='flex justify-center'>
-    <div className='max-w-md h-screen flex flex-col items-center items-stretch gap-4'>
-      <header className='basis-0 text-center text-3xl font-bold'>Sudoku</header>
+    <div className='w-11/12 h-screen flex flex-col items-center items-stretch gap-4'>
+      <header className='pt-4 basis-0 text-center text-3xl font-bold'>Sudoku</header>
       <div className='flex justify-center'>
         <Grid />
       </div>
-      <Input className='grow' />
+      <Input />
     </div>
   </div>
 )
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { newGame, enter } from '../reducers/clueReducer'
 
-const Input = ({ className }) => {
+const Input = () => {
   const input = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   const dispatch = useDispatch()
 
@@ -14,21 +14,19 @@ const Input = ({ className }) => {
     dispatch(enter({ value: target.textContent }))
   }
   return (
-    <div className={className}>
-      <div className='flex place-content-around items-start'>
-        <button
-          type='button'
-          onClick={handleNewGame}
-        >
-          New Game
-        </button>
-        <div className='grid grid-cols-3 grid-rows-3 border'>
-          {input.map((num) => (
-            <button type='button' onClick={handleClick} className='h-8 w-8' key={num}>
-              {num}
-            </button>
-          ))}
-        </div>
+    <div className='flex place-content-around items-start'>
+      <button
+        type='button'
+        onClick={handleNewGame}
+      >
+        New Game
+      </button>
+      <div className='w-1/3 aspect-square grid grid-cols-3 grid-rows-3 border'>
+        {input.map((num) => (
+          <button type='button' onClick={handleClick} key={num}>
+            {num}
+          </button>
+        ))}
       </div>
     </div>
   )
